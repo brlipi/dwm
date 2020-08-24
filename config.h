bf46e6e4fa3321d29e193bfc3aadd6bb7bc7c764
Breno Lipi <brenorlipi@hotmail.com>
2020-08-24 17:20:30 -0300

    reverting files back to no emoji support

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating   = 0;  /* 1 means swallow floating windows by default */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "EmojiOne:pixelsize=12:antialias=true:autohint=true" };
+static const char *fonts[]          = { "monospace:size=10"};
 static const char dmenufont[]       = "monospace:size=10";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
